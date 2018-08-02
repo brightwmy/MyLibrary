1c272a5c706956e0723611ff51c446e7cc395e7c
吴明云 <wumingyun@p1.com>
2018-08-02 16:12:45 +0800

    initial

---
+++ MyLibrary/Classes/Print.m
@@ -0,0 +1,17 @@
+//
+//  Print.m
+//  OMI
+//
+//  Created by wumingyun on 2018/7/22.
+//  Copyright © 2018 OMI. All rights reserved.
+//
+
+#import "Print.h"
+
+@implementation Print
+
+- (void)print {
+    NSLog(@"-- my library, hello world");
+}
+
+@end
